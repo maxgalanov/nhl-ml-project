51ee3a31b3f9febefbebe29d2c29e27f338c1f2e
Victoria Shiryaeva <vi.shiryaevva@gmail.com>
2024-04-20 22:39:17 +0300

    Edit cron expression

--- nhl_project/src/data/dwh/teams_stat_parser.py
+++ nhl_project/src/data/dwh/teams_stat_parser.py
@@ -23,7 +23,7 @@ DEFAULT_ARGS = {
 
 dag = DAG(
     dag_id="nhl_teams_stat",
-    schedule_interval="45 5 1 10 *",
+    schedule_interval="0 18 * * *",
     start_date=days_ago(2),
     catchup=False,
     tags=["hse_nhl_ml_project"],
